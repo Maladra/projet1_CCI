2770e69bba50e862574ad55444b48942795cef55
Maladra <julien.ehles@hotmail.fr>
2018-08-19 20:26:16 +0200

    premier projet CCI
    ajout de mon premier projet de la CCI

---
+++ Projet1/Projet1/Controllers/SondageController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projet1.Models;
+using System.Data.SqlClient;
+
+namespace Projet1.Controllers
+{
+    public class SondageController : Controller
+    {
+        //Afficher la vue de création d'un sondage
+        public ActionResult CreerSondage()
+        {
+            return View("CreerSondage");
+        }
+
+
+        //Enregistrement d'un sondage dans la BDD
+        public ActionResult SondageCree(string question, List<string> Choix, string check)
+        {
+
+            //Les choix vides sont effacés de la liste "Choix" avant qu'elle ne soit enregistrée dans la BDD
+            Choix.RemoveAll(string.IsNullOrEmpty);
+
+            //Le FOR et le IF suivants empêchent la création d'un sondage 
+            //si le champ "QUESTION" est vide ou si l'utilisateur n'entre pas au moins 2 choix
+            int choixVide = 0;
+
+            for (int i = 0; i < Choix.Count; i++)
+            {
+                if (Choix[i] == "")
+                {
+                    choixVide++;
+                }
+            }
+
+
+            if (question == ""||choixVide>2)
+            {
+                return View("NbQuestionEtChoixMin"); //Si les conditions précédentes ne sont pas remplies, 
+            }                                        //l'utilisateur est renvoyé vers une page d'erreur
+            else
+            {
+
+                //Vérifie si les choix multiples sont autorisés pour le sondage
+                bool choixMultiple;
+                if (check == "checked")
+                { choixMultiple = true; }
+                else choixMultiple = false;
+
+                Guid generationCleDesactivation = Guid.NewGuid(); //Génération d'une valeur aléatoire de type Guid pour le lien de désactivation du sondage
+                string cleDesactivation = generationCleDesactivation.ToString(); //Convertie la valeur du Guid en string
+
+                
+                Sondage NouveauSondage = new Sondage(question, Choix, choixMultiple, cleDesactivation);
+
+                ViewBag.dernierIDBDD = SQL.SauvegarderEnBDD(NouveauSondage); //Appel de la méthode permettant de sauvegarder le sondage dans la BDD
+                                                                             //et récupération de l'ID du sondage en cours
+
+                
+
+                ViewBag.cleDesactivation = cleDesactivation;
+
+
+                return View("SondageCree");
+            }
+
+        }
+
+        public ActionResult Sondage(int Id)
+        {
+            ViewBag.idSondage = Id;
+
+            // On vérifie si le sondage n'a pas été précédemment désactivé
+            QuestionEtChoix QuestionEtChoixBDD = SQL.GetQuestionEtChoix(Id);
+            if (QuestionEtChoixBDD.sondageActif == true)
+            {
+                return View("Sondage", QuestionEtChoixBDD);
+            }
+            else
+            {
+                return View("SondageDesactive");
+            }
+
+        }
+
+
+
+        public ActionResult Voter(int idSondage, List<int> ChoixSondage)
+        {
+            ViewBag.idSondage = idSondage;
+
+            if (ChoixSondage != null) //Permet de vérifier que l'utilisateur a sélectionné au moins une réponse
+            {
+                HttpCookie MyCookie = Request.Cookies[idSondage.ToString()]; //Verifie la présence de l'id du sondage en string dans le Cookie
+
+                if (MyCookie == null)
+                {
+
+                    MyCookie = new HttpCookie(idSondage.ToString()); //Si le vote de l'utilisateur est possible, génération d'un cookie,
+                                                                     //besoin d'une conversion de l'id du sondage en string
+                    Response.Cookies.Add(MyCookie); //Ajout du cookie dans le navigateur de l'utilisateur
+                    SQL.Voter(idSondage, ChoixSondage);
+                    return View("Vote");
+                }
+                else
+                {
+                    return View("DejaVote");
+                }
+
+            }
+            else
+            {
+                return View("Erreur");
+            }
+
+        }
+
+        public ActionResult Resultats(int Id)
+        {
+            ResultatsSondage ResultatsBDD = SQL.GetResultats(Id);
+
+            int nbDeVoixTotal = 0;
+            
+            foreach(int nb in ResultatsBDD.NbDeVotantsParChoix)
+            {
+                nbDeVoixTotal += nb;
+                
+            }
+            ViewBag.nbDeVoixTotal = nbDeVoixTotal; 
+                        
+            return View("Resultat", ResultatsBDD);
+        }
+
+        public ActionResult SondageDesactive(int Id, string key)
+        {
+            string keyBDD = SQL.VerificationCleDesactivation(Id, key);
+
+            if(keyBDD == key)
+            {
+                SQL.DesactivationSondage(Id);
+
+                return View("SondageSupprime");
+            }
+            else
+            {
+                
+                return View("ErreurSondageSupprime");
+            }
+            
+        }
+
+    }
+}
---
+++ Projet1/Projet1/Models/Sondage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projet1.Models
+{
+    public class Sondage
+    {
+        
+        public string Question { get; set; }
+        public List<string> Choix { get; set; }
+        public bool ChoixMultiple { get; set; }
+        public string LienSuppression { get; set; }
+        public string LienResultat { get; set; }
+        public string LienPartage { get; set; }
+        public bool SondageActif { get; set; }
+        public int NbVotantsParChoix { get; set; }
+
+        public Sondage(string _Question, List<string> _Choix, bool _choixMultiple, string _lienSuppression)
+        {
+            
+            Question = _Question;
+            Choix = _Choix;
+            ChoixMultiple = _choixMultiple;
+            LienSuppression = _lienSuppression;
+           
+
+        }
+
+    }
+    
+
+
+    
+    
+
+}
